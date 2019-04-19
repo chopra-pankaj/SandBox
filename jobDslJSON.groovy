5cf35600beaa650b1d668b58d8bb5712b1873ab6
SeritAndrei <46908172+SeritAndrei@users.noreply.github.com>
2019-04-19 18:54:45 +0300

    Update jobDslJSON.groovy

--- jobDslJSON.groovy
+++ jobDslJSON.groovy
@@ -5,13 +5,9 @@ def reader = new java.io.FileReader("${workspace}/test.json")
 def dslJobs = new JsonSlurper().parse(reader)
 
 dslJobs.item.each {
-  job("${it.projectname}") {
-    scm {
-      git("${it.scm}")
-    }
-    
+  job("${it.projectname}") {  
     steps {
-      shell("cat README.md")
+      shell('echo $JOB_NAME')
     }
   }
 }
